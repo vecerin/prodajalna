bafd42f0a3864bee1f25f6bfb24e02de6766347a
David <david.javornik.vecerin@gmail.com>
2016-05-08 20:33:22 +0000

    Zaključek dela na I. nalogi

--- eracun.js
+++ eracun.js
@@ -208,14 +208,27 @@ streznik.post('/prijava', function(zahteva, odgovor) {
     	  Address, City, State, Country, PostalCode, \
     	  Phone, Fax, Email, SupportRepId) \
         VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
-      //TODO: add fields and finalize
-      //stmt.run("", "", "", "", "", "", "", "", "", "", "", 3); 
-      //stmt.finalize();
+      stmt.run(polja.FirstName, polja.LastName, polja.Company, polja.Address, polja.City, polja.State, polja.Country, polja.PostalCode, polja.Phone, polja.Fax, polja.Email, 3); 
+      stmt.finalize();
     } catch (err) {
       napaka2 = true;
     }
   
-    odgovor.end();
+    var sporocilo2;
+    if(napaka2) {
+      sporocilo2 = "Prišlo je do napake pri registraciji nove stranke. Prosim preverite vnešene podatke in poskusite znova.";
+    }
+    else {
+      sporocilo2 = "Stranka je bila uspešno registrirana.";
+    }
+    
+    vrniStranke(function(napaka1, stranke) {
+      vrniRacune(function(napaka2, racuni) {
+        odgovor.render('prijava', {sporocilo: sporocilo2, seznamStrank: stranke, seznamRacunov: racuni});  
+      }) 
+    });
+    
+    //odgovor.end();
   });
 })
 
